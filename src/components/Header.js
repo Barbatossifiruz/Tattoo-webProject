a5414f0e3c90d6cadc3758e9bc8d89020ae9595e
BarbatosSifiruz <makotohishigami@gmail.com>
2023-10-09 16:48:04 -0300

    Add new classes and styles on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
         <header
             className={`${
                 isActive ? "h-[100px] lg:h-[110px] shadow-lg" : "  h-[120px] lg:h-[150px]"
-            } fixed left-0 right-0 z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}
+            } fixed left-0 bg-white right-0 z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}
         >
             <div className="flex justify-between items-center h-full pl-[50px] pr-[60px]">
                 {/* Logo */}
@@ -42,12 +42,23 @@ const Header = () => {
                     <Nav />
                 </div>
                 {/* Nav menu btn- showing by defoult - hidden on desktop mode */}
-                <div className="xl:hidden absolute right-[5%] bg-dark text-white p-2 rounded-md cursor-pointer">
+                <div
+                    onClick={() => setNavMobile(!navMobile)}
+                    className="xl:hidden absolute right-[5%] bg-dark text-white p-2 rounded-md cursor-pointer"
+                >
                     <TiThMenuOutline className="text-3xl" />
                 </div>
                 {/* Nav mobile - showing by default - hidden on desktop*/}
-                <div className="fixed bg-red-500 w-full h-full left-0 -z-10 transition-all duration-300">
+                <div
+                    className={`${navMobile ? "max-h-full" : "max-h-0"} 
+                                ${isActive ? "top-[100px] lg:top-[110px]" : "top-[120px] lg:top-[150px]"} 
+                    fixed bg-white w-full h-full left-0 -z-10 transition-all duration-300`}
+                >
                     <NavMobile />
+                </div>
+                {/* Social icons - initially hidden - show on desktop */}
+                <div className="hidden xl:flex">
+                    <Socials />
                 </div>
             </div>
         </header>
