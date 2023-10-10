5bb3fcdd6620676652f39f2998d1ecab31a69f78
BarbatosSifiruz <makotohishigami@gmail.com>
2023-10-10 13:52:35 -0300

    Finish the section about

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-// import about Data
+//* import about Data
 import { aboutData } from "../data";
 
-// Import motion
+//* Import motion
 import { motion } from "framer-motion";
 
-// Import variants
+//* Import variants
 import { fadeIn } from "../variants";
 
 const About = () => {
-    // Destructure about Data
+    //* Destructure about Data
     const { title, subtitle1, subtitle2, btnText, btnIcon } = aboutData;
 
     return (
@@ -18,24 +18,36 @@ const About = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col lg:flex-row gap-x-[70px] items-center">
                     {/* Numbers */}
-                    <div className="flex flex-col lg:flex-row flex-1">
-                        <div className="text-[360px] xl:text-[720px] leading-none font-tertiary lg:tracking-[0.055em] bg-about bg-no-repeat bg-right bg-clip-text text-transparent">
+                    <motion.div
+                        variants={fadeIn("right")}
+                        initial="hidden"
+                        whileInView={"show"}
+                        viewport={{ once: false, amount: 0.7 }}
+                        className="flex flex-col lg:flex-row flex-1"
+                    >
+                        <div className="text-[360px] xl:text-[720px] leading-none font-tertiary lg:-tracking-[0.055em] bg-about bg-no-repeat bg-right bg-clip-text text-transparent">
                             01
                         </div>
-                    </div>
+                    </motion.div>
                     {/* Text */}
-                    <div className="flex-1 h-full xl:mt-48">
+                    <motion.div
+                        variants={fadeIn("left")}
+                        initial="hidden"
+                        whileInView={"show"}
+                        viewport={{ once: false, amount: 0.6 }}
+                        className="flex-1 h-full xl:mt-48"
+                    >
                         <h2 className="h2">{title}</h2>
-                        <div>
-                            <div>
-                                <p>{subtitle1}</p>
-                                <p>{subtitle2}</p>
-                                <button>
-                                    {btnText} <div>{btnIcon}</div>
+                        <div className="flex flex-col items-center ">
+                            <div className="max-w-[530px] text-grey">
+                                <p className="mb-6">{subtitle1}</p>
+                                <p className="mb-9">{subtitle2}</p>
+                                <button className="btn btn-lg btn-link">
+                                    {btnText} <div className="text-xl">{btnIcon}</div>
                                 </button>
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
