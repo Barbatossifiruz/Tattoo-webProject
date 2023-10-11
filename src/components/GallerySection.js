e0c1b4be5617f72d7acb21055d855cc68265a7b0
BarbatosSifiruz <makotohishigami@gmail.com>
2023-10-11 11:12:07 -0300

    Complete the secction gallery

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -17,7 +17,7 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
 //* Slides
-const Slides = galleryData.images.map(({ original, width, height }) => ({
+const slides = galleryData.images.map(({ original, width, height }) => ({
     src: original,
     width,
     height,
@@ -25,10 +25,55 @@ const Slides = galleryData.images.map(({ original, width, height }) => ({
 
 const GallerySection = () => {
     //! Index state
-    const [Index, setIndex] = useState(-1);
+    const [index, setIndex] = useState(-1);
     //! Destructure gallery data
     const { title, btnText, btnIcon, images } = galleryData;
-    return <section>GallerySection</section>;
+    return (
+        <section className="bg-[#f9f9f9] section relative mt-[40px] lg:mt-0">
+            <div className="container mx-auto">
+                <motion.h2
+                    variants={fadeIn("up")}
+                    initial="hidden"
+                    whileInView={"show"}
+                    viewport={{ once: false, amount: 0.6 }}
+                    className="h2 max-w-[370px] lg:mb-20"
+                >
+                    {title}
+                </motion.h2>
+            </div>
+            {/* Photo album */}
+            <motion.div
+                variants={fadeIn("up")}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.2 }}
+                className="mb-8 lg:mb-20"
+            >
+                <PhotoAlbum onClick={(event, photo, index) => setIndex(index)} layout="rows" photos={images} />
+                <Lightbox
+                    slides={slides}
+                    styles={{ container: { backgroundColor: "rgba(0,0,0, .9)" } }}
+                    open={index >= 0}
+                    index={index}
+                    close={() => setIndex(-1)}
+                />
+            </motion.div>
+            {/* Btn */}
+            <motion.div
+                variants={fadeIn("up")}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.2 }}
+                className="flex justify-center"
+            >
+                <button className="btn btn-lg btn-dark">
+                    {btnText} <div className="text-xl">{btnIcon}</div>
+                </button>
+            </motion.div>
+        </section>
+    );
 };
 
 export default GallerySection;
+
+// 43:37
