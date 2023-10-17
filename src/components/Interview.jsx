dd1ac3a820a63b54de2087f64dde84ab699b2787
BarbatosSifiruz <makotohishigami@gmail.com>
2023-10-17 09:25:15 -0300

    Add new section to Develop branch

--- src/components/Interview.jsx
+++ src/components/Interview.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+
+//* Import data
+import { interviewData } from "../data";
+
+//* Import modal video
+import ModalVideo from "react-modal-video";
+
+//* Modal video Css
+import "../modalVideo.scss";
+
+//* Import motion
+import { motion } from "framer-motion";
+
+//* Import fade in
+import { fadeIn } from "../variants";
 
 const Interview = () => {
-  return <div>Interview</div>;
+    //* Destructure interview data
+    const { title, btnText, btnIcon } = interviewData;
+    //* Open state
+    const [isOpen, setIsOpen] = useState(false);
+
+    return <section className="">Interview</section>;
 };
 
 export default Interview;
