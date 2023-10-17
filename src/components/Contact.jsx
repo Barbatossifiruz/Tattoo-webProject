98fec6deb429efa1b0986f257a7165be40ee1fce
BarbatosSifiruz <makotohishigami@gmail.com>
2023-10-17 10:59:48 -0300

    Add imports to the component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+//* Import contact data
+import { contactData } from "../data";
+
+//* Import Motion
+import { motion } from "framer-motion";
+
+//* Import fade In
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div>Contact</div>;
+    return <div>Contact</div>;
 };
 
 export default Contact;
