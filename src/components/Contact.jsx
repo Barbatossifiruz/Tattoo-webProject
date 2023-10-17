4e43da7f3e02c45c08d145023a9f7ea8d183bdf4
BarbatosSifiruz <makotohishigami@gmail.com>
2023-10-17 12:33:23 -0300

    Add form  to the component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,13 +2,77 @@
 import { contactData } from "../data";
 
 //* Import Motion
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 //* Import fade In
-import { fadeIn } from "../variants";
+// import { fadeIn } from "../variants";
 
 const Contact = () => {
-    return <div>Contact</div>;
+    //? Destructure contact data
+    const { title, info, form } = contactData;
+    return (
+        <>
+            <section className="section">
+                <div className="container mx-auto">
+                    <div className="flex flex-col xl:flex-row gap-y-16">
+                        {/* text */}
+                        <div className="flex-1">
+                            {/* title */}
+                            <h2 className="h2 max-w-[490px]">{title}</h2>
+                            {/* info items */}
+                            <div className=" flex flex-col xl:flex-row gap-x-5 gap-y-16 xl:gap-y-0">
+                                {info.map((item, index) => {
+                                    // Destructure item
+                                    const { title, subtitle, address, phone, email, link } = item;
+                                    return (
+                                        <div key={index}>
+                                            {/* title */}
+                                            <div className="font-primary uppercase font-medium text-xl mb-3">{title}</div>
+                                            {/* subtitle */}
+                                            <div className="mb-6 text-[333] leading-[187%] tracking-[0.02em]">{subtitle}</div>
+                                            {/* address, phone & email */}
+                                            <div className="flex flex-col gap-y-3 mb-8">
+                                                {/* Address */}
+                                                <div className="flex items-center gap-[10px]">
+                                                    <div>{address.icon}</div>
+                                                    <div className="font-medium">{address.name}</div>
+                                                </div>
+                                                {/* Phone */}
+                                                <div className="flex items-center gap-[10px]">
+                                                    <div>{phone.icon}</div>
+                                                    <div className="font-medium">{phone.number}</div>
+                                                </div>
+                                                {/* Email */}
+                                                <div className="flex items-center gap-[10px]">
+                                                    <div>{email.icon}</div>
+                                                    <div className="font-medium">{email.address}</div>
+                                                </div>
+                                            </div>
+                                            {/* Link */}
+                                            <a className="font-medium border-b border-dark pb-[5px]" href="/">
+                                                {link}
+                                            </a>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        {/* form */}
+                        <div className="bg-pink-200">
+                            <form className="flex flex-col">
+                                <input
+                                    className="border-b border-dark placeholder:text-[#555] italic tracking-[0.06em] outline-none pb-4"
+                                    placeholder={form.name}
+                                    type="text"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            ;
+        </>
+    );
 };
 
 export default Contact;
