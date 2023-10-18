f57e160a336fe8ed706059c72f0bb242c2e0d10a
BarbatosSifiruz <makotohishigami@gmail.com>
2023-10-18 14:22:09 -0300

    Config new styles in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    fontFamily: {
-      primary: 'Oswald',
-      secondary: 'Raleway',
-      tertiary: 'Rozha One',
-    },
-    container: {
-      padding: {
-        DEFAULT: '20px',
-        lg: '0',
-      },
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1300px',
-    },
-    extend: {
-      colors: {
-        dark: '#111111',
-        grey: {
-          DEFAULT: '#555555',
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        fontFamily: {
+            primary: "Oswald",
+            secondary: "Raleway",
+            tertiary: "Rozha One",
         },
-      },
-      backgroundImage: {
-        hero: "url('/src/img/hero/bg.jpg')",
-        about: "url('/src/img/about/image.png')",
-        interview: "url('/src/img/interview/bg.png')",
-      },
-      content: {
-        outlineText: 'url("/src/img/hero/outline-text.svg")',
-      },
+        container: {
+            padding: {
+                DEFAULT: "20px",
+                lg: "0",
+            },
+        },
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1300px",
+        },
+        extend: {
+            colors: {
+                dark: "#111111",
+                greyLight: "#dbdbdb",
+                grey: {
+                    DEFAULT: "#555555",
+                },
+            },
+            backgroundImage: {
+                hero: "url('/src/img/hero/bg.jpg')",
+                about: "url('/src/img/about/image.png')",
+                interview: "url('/src/img/interview/bg.png')",
+            },
+            content: {
+                outlineText: 'url("/src/img/hero/outline-text.svg")',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
